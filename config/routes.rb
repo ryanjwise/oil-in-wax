470037119070a21f39581f35ab59a0c1eed81e30
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-05-18 22:52:35 +1000

    Homepage placeholder scaffolded

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root to: 'home#index'
   
-  get 'home/index'
+  get 'home/index', to: 'home#index', as: 'home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
