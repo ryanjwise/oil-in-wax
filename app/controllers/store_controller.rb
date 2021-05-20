67ded7c3146e45858ffc3e8610500343820cbcb0
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-05-20 20:59:43 +1000

    Add Store CRUD

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,41 +1,51 @@
 class StoreController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :admin]
+  before_action :authenticate_user!, only: [:new, :admin, :edit, :update, :destroy]
+  before_action :set_store, only: [:admin, :edit, :update, :destroy]
+
   def new
     @store = Store.new
   end
 
   def show
-
   end
-
+  
   def admin
+    @candles = @store.candles
 
   end
 
   def create
-    @store = current_user.store.new(store_params)
+    @store = current_user.build_store(store_params)
     if @store.save
-      redirect_to @store
+      redirect_to store_admin_path
     else
       render :new
     end
   end
 
   def edit
-
+    
   end
 
   def update
-
+    if @store.update(store_params)
+      redirect_to store_admin_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @listing.destroy
+    @store.destroy
     flash[:alert] = "Succesfully Destroyed"
     redirect_to home_path
   end
 
   private
+
+  def set_store
+    @store = current_user.store
+  end
 
   def store_params
     params.require(:store).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'store/new'
-  get 'store/show'
-  root to: 'home#index'
-  
+  get 'store/new',  to: 'store#new',  as: 'store_create'
+  post '/stores',   to: 'store#create'
+  get 'store/edit',  to: 'store#edit', as: 'store_edit'
+  patch '/store',  to: 'store#update'
+  delete '/store', to: 'store#destroy'
+
+  get 'store/show', to: 'store#show', as: 'store_show'
+  get 'store/admin', to: 'store#admin', as: 'store_admin'
   get 'home/index', to: 'home#index', as: 'home'
+  
+  root to: 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
