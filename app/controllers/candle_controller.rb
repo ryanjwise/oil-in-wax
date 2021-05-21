2cb61e5106318bdfa1a034955cd4bea5844848c1
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-05-21 20:00:23 +1000

    Add Final CRUD elements for store

---
+++ app/controllers/candle_controller.rb
@@ -0,0 +1,51 @@
+class CandleController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :set_candle, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @candle = current_user.store.candles.new(candle_params)
+    if @candle.save
+      redirect_to store_admin_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @candle = Candle.new
+  end
+
+  def destroy
+    @candle.destroy
+    flash[:alert] = "Succesfully Deleted"
+    redirect_to store_admin_path
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+    parameters = candle_params
+    parameters[:ingredients] = parameters[:ingredients].split
+    parameters[:scents] = parameters[:scents].split
+    if @candle.update(parameters)
+      redirect_to store_admin_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_candle
+    @candle = Candle.find(params[:id])
+  end
+
+  def candle_params
+    params.require(:candle).permit(:name, :description, :stock, :price, :scents, :ingredients)
+  end
+end
---
+++ app/helpers/candle_helper.rb
@@ -0,0 +1,2 @@
+module CandleHelper
+end
